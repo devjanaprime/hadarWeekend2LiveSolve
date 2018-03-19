96cb101332655914d88a288cc9d6f1d2e3169fb1
devjanaprime <dev@primeacademy.io>
2018-03-19 09:57:22 -0500

    interface and client side started

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,21 @@
-console.log( 'js' );
+$( document ).ready( readyNow ); //end doc ready
 
-$( document ).ready( function(){
-    console.log( 'JQ' );
-}); //end doc ready
+function readyNow(){
+    $( '#clearButton' ).on( 'click', function(){
+        console.log( 'in clearButton on click' );
+    }); // end clearButton on click
+
+    $( '#doMathButton' ).on( 'click', function(){
+        console.log( 'in doMathButton on click' );
+        // capture user input & store in an object
+        let objectToSend = {
+            x: $( '#xIn' ).val(),
+            y: $( '#yIn' ).val(),
+            type: $( '#typeIn' ).val()
+        }; // end objectToSend
+        console.log( 'objectToSend:', objectToSend ); 
+        // send object to server with AJAX
+        // display answer on DOM
+    }); // end doMathButton on click
+
+} // end ready now
