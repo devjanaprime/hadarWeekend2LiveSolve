c22cd02598344924d2c3b429e8da3a11f3653fa7
devjanaprime <dev@primeacademy.io>
2018-03-19 09:30:29 -0500

    first!

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// requires
+let express = require( 'express' );
+let app = express();
+let bodyParser = require( 'body-parser' );
+
+// uses
+app.use( bodyParser.urlencoded( { extended: true } ) );
+// look server/public 1st 
+app.use( express.static( 'server/public' ) );
+
+// vars
+let port = 4567;
+
+// spin up server
+app.listen( port, function(){
+    console.log( 'server up on:', port );
+}); // end spin up server
+
